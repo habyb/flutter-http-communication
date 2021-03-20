3e0b89d3a3c6a80aed5f1cfce66328f34a16b5f3
Habyb Fernandes <habybfernandes@gmail.com>
2021-03-20 19:57:23 -0300

    identifying web api errors

--- lib/screens/transaction_form.dart
+++ lib/screens/transaction_form.dart
@@ -100,6 +100,8 @@ class _TransactionFormState extends State<TransactionForm> {
       if (transaction != null) {
         Navigator.pop(context);
       }
+    }).catchError((e){
+      print(e);
     });
   }
 }
